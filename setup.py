da79ee4426e37381f51cca64e774bcf06b34fb70
Simon Eggler <simon.eggler@softbee.ch>
2018-03-22 22:44:39 +0100

    update documenation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='jsog3',
-	version='2.0.3',
+	version='2.0.4',
     packages=['jsog3'],
 	author='Simon Eggler',
 	author_email='simon.eggler@gmx.net',
